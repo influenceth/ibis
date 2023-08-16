dbecc896785e297a16c4ff25db4973c06b6ee0b7
Chris Lexmond <chris@chrislexmond.com>
2023-08-16 16:43:43 -0400

    Fixed casing issue

--- src/lib/Contract.js
+++ src/lib/Contract.js
@@ -6,12 +6,12 @@ class Contract extends StarknetContract {
   }
 
   // Extend Starknet.js to allow calls with calldata as an object
-  async compileAndcall(method, calldata, options) {
+  async compileAndCall(method, calldata, options) {
     return await super.call(method, this.callData.compile(method, calldata), options);
   }
 
   // Extend Starknet.js to allow invokes with calldata as an object
-  async compileAndinvoke(method, calldata, options) {
+  async compileAndInvoke(method, calldata, options) {
     return await super.invoke(method, this.callData.compile(method, calldata), options);
   }
 }
