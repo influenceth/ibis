3290ffe6dd1586eb906af5d9e2aa10f3b2f42570
Chris Lexmond <chris@chrislexmond.com>
2023-08-16 14:49:09 -0400

    Added calls with object calldata

--- src/lib/Contract.js
+++ src/lib/Contract.js
@@ -5,9 +5,15 @@ class Contract extends StarknetContract {
     super(...args);
   }
 
-  async invoke(method, _calldata, options) {
-    let calldata = _calldata;
-    if (typeof args === 'object') calldata = this.callData.compile(method, _calldata);
+  // Extend Starknet.js to allow calls with calldata as an object
+  async call(method, calldata, options) {
+    if (typeof calldata === 'object') calldata = this.callData.compile(method, calldata);
+    return await super.call(method, calldata, options);
+  }
+
+  // Extend Starknet.js to allow invokes with calldata as an object
+  async invoke(method, calldata, options) {
+    if (typeof calldata === 'object') calldata = this.callData.compile(method, calldata);
     return await super.invoke(method, calldata, options);
   }
 }
